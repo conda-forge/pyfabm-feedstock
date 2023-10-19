a2c611e12e3d05980be177a2da4227280bf5e067
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-10-19 09:08:24 +0100

    pass CMAKE_ARGS; test compiled extension

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,6 @@
 set FFLAGS=-static-libgfortran
 set LDFLAGS=-static
 set "CMAKE_GENERATOR=MinGW Makefiles"
+echo [build_ext]>"%SRC_DIR%\setup.cfg"
+echo cmake_opts=%CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
 "%PYTHON%" -m pip install . -vv
