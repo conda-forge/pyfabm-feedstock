7470aa0faeae6c8ac380d97b28c20443b1a82a0f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-10-08 09:50:02 +0100

    use new ninja and gfortran on windows; drop patches for old gfortran; add setuptools as per conda-forge linter

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,8 @@
+REM From https://github.com/conda-forge/netcdf-fortran-feedstock/blob/main/recipe/bld.bat
+set "HOST=x86_64-w64-mingw32"
+set "FC=%HOST%-gfortran.exe"
+
 echo [build_ext]>"%SRC_DIR%\setup.cfg"
-echo cmake_opts=-G "MinGW Makefiles" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
+echo cmake_opts=-G "Ninja" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
+echo compiler=%FC%>>"%SRC_DIR%\setup.cfg"
 "%PYTHON%" -m pip install . -vv
